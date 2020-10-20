061c3f26d110c88a652a2be9e242c5cbe46916a7
Megan Anderson <kyachimegann@gmail.com>
2020-10-19 23:05:43 -0500

    30 days day 14 - scope.

---
+++ HackerRankExamples/30DaysDay14Scope.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankExamples
+{
+    class _30DaysDay14Scope
+    // https://www.hackerrank.com/challenges/30-scope/problem
+    // Too lazy to copy whole description
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int[] list)
+        {
+            elements = list;
+        }
+        // Add your code here
+        public void computeDifference()
+        {
+            int eleLength = elements.Length;
+            Array.Sort(elements);
+            maximumDifference = elements[eleLength - 1] - elements[0];
+        }
+
+    } // End of Difference Class
+
+}
